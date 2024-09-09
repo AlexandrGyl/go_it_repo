d419f36d9d5b1bf1bffd76edba7ad5fc4153587b
Alexander Gyl <defakto.gylop@gmail.com>
2024-09-09 22:26:47 +0300

    MyFunc

---
+++ Speed_2.py
@@ -0,0 +1,12 @@
+#comment
+print ("Alexander")
+a=int(input ("Ведіть А"))
+b=int(input ("Введіть Б"))
+def myFunction (a, b):
+    b = a + 1
+    c = a + b
+    return c
+
+result=myFunction(a, b)
+
+print (result)
